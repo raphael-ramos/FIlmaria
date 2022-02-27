0c44dd10c9b3bd7843da47ab3442a7e100526fc0
raphael-ramos <raphael.ramos95@hotmail.com>
2022-02-27 17:47:02 -0300

    Finalizado os estilos da página filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate} from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 import api from '../../services/api';
@@ -11,27 +11,47 @@ export default function Filme(){
     const [loading, setLoading] = useState(true);
     const {id} = useParams();
 
+    const navigate = useNavigate()
+
     useEffect(()=>{
         async function loadFilme(){
             const response = await api.get(`r-api/?api=filmes/${id}`);
+
+            //verifica se existe item com o id informado na url, caso não exista na api é redirecionado  para a home
+            if(response.data.length === 0){
+                navigate('/');
+                return;
+            }
             // console.log(response.data);
             setFilme(response.data);
             setLoading(false);
         }
 
         loadFilme();
-    },[id])
+        return()=>{
+            console.log("componente desmontado")
+        }
+    },[id, navigate])
 
     if(loading){
         return(
             <div className="filme-info">
-                <h1>Carregando filme</h1>
+                <h1>Carregando filme...</h1>
             </div>
         )
     }
     return(
         <div className="filme-info">
-            <h1>Página detalhes - {filme.nome}</h1>
+            <h1>{filme.nome}</h1>
+            <img src={filme.foto} alt={filme.nome}/>
+            <h3>Sinopse</h3>
+            <p>{filme.sinopse}</p>
+            <div className='botoes'>
+                <button onClick={()=>{}}>Salvar</button>
+                <a target="blank" href={`https://www.youtube.com/results?search_query=${filme.nome} Trailer`}>
+                    Trailer
+                </a>
+            </div>
         </div>
     )
 }
